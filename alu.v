0727e5f49d3a1a823b2dba6ced1834b04e575d12
Simon Yin <simonyin09@gmail.com>
2026-03-07 15:37:58 -0600

    optimized ALU, using a single 33 bit subtrator for subtractions and all comparisons, as well as a universal shifter

--- alu.v
+++ alu.v
@@ -9,7 +9,7 @@ module alu (
   output wire        unsigned_less_than_o  //unsigned less than flag (for BLTU/BGEU)
 );
 
-  // operation encodings funct3 + modifier
+  //operation encodings funct3 + modifier
   localparam [3:0] ALU_ADD  = 4'b0000, 
                    ALU_SLL  = 4'b0001, 
                    ALU_SLT  = 4'b0010, 
@@ -22,15 +22,39 @@ module alu (
                    ALU_SRA  = 4'b1101, 
                    ALU_LUI  = 4'b1010; 
 
+  //function to reverse bits for the universal shifter
+  function [31:0] reverse_32;
+    input [31:0] x;
+    integer i;
+    begin
+      for (i = 0; i < 32; i = i + 1) begin
+        reverse_32[i] = x[31 - i];
+      end
+    end
+  endfunction
+
+  //single 33-bit subtractor to handle subtraction and all comparisons
+  wire [32:0] alu_minus_33 = {1'b0, src_a_i} - {1'b0, src_b_i};
+  assign less_than_o = (src_a_i[31] ^ src_b_i[31]) ? src_a_i[31] : alu_minus_33[32]; 
+  //less than flag checks sign bits first, then checks if result of subtraction is negative
+  assign unsigned_less_than_o = alu_minus_33[32]; //if subtraction is negative, then A < B(unsigned)
+
+  //universal shifter logic
+  wire        is_shift_left  = (alu_control_i == ALU_SLL);
+  wire        is_arith_shift = (alu_control_i == ALU_SRA);
+  wire [31:0] shifter_in     = is_shift_left ? reverse_32(src_a_i) : src_a_i;
+  wire [31:0] shifter        = $signed({is_arith_shift & src_a_i[31], shifter_in}) >>> src_b_i[4:0];
+  wire [31:0] left_shift     = reverse_32(shifter);
+
   always @(*) begin
     case (alu_control_i)
       ALU_ADD:  alu_result_o = src_a_i + src_b_i;
-      ALU_SUB:  alu_result_o = src_a_i - src_b_i;
-      ALU_SLL:  alu_result_o = src_a_i << src_b_i[4:0];
-      ALU_SRL:  alu_result_o = src_a_i >> src_b_i[4:0];
-      ALU_SRA:  alu_result_o = $signed(src_a_i) >>> src_b_i[4:0];
-      ALU_SLT:  alu_result_o = ($signed(src_a_i) < $signed(src_b_i)) ? 32'd1 : 32'd0;
-      ALU_SLTU: alu_result_o = (src_a_i < src_b_i) ? 32'd1 : 32'd0;
+      ALU_SUB:  alu_result_o = alu_minus_33[31:0];
+      ALU_SLL:  alu_result_o = left_shift;
+      ALU_SRL:  alu_result_o = shifter;
+      ALU_SRA:  alu_result_o = shifter;
+      ALU_SLT:  alu_result_o = {31'd0, less_than_o};
+      ALU_SLTU: alu_result_o = {31'd0, unsigned_less_than_o};
       ALU_XOR:  alu_result_o = src_a_i ^ src_b_i;
       ALU_OR:   alu_result_o = src_a_i | src_b_i;
       ALU_AND:  alu_result_o = src_a_i & src_b_i;
@@ -39,8 +63,6 @@ module alu (
     endcase
   end
 
-  assign zero_o               = (alu_result_o == 32'd0); //zero flag
-  assign less_than_o          = ($signed(src_a_i) < $signed(src_b_i)); //less than flag for signed comparison 
-  assign unsigned_less_than_o = (src_a_i < src_b_i); //unsigned less than flag 
+  assign zero_o = (alu_result_o == 32'd0); 
 
 endmodule
